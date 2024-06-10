15dc76cc90a07a5627bf0b5dcedc3e41d7852c48
James Aguilar <799564+jaguilar@users.noreply.github.com>
2024-06-09 22:40:26 -0400

    Add Borrowable type inspired by Pigweed

--- src/freertosxx/include/freertosxx/mutex.h
+++ src/freertosxx/include/freertosxx/mutex.h
@@ -3,6 +3,11 @@
 
 #include <FreeRTOSConfig.h>
 
+#include <memory>
+#include <optional>
+#include <type_traits>
+#include <utility>
+
 #include "FreeRTOS.h"
 #include "projdefs.h"
 #include "semphr.h"
@@ -15,9 +20,11 @@ class Mutex {
   Mutex();
   ~Mutex();
   Mutex(const Mutex&) = delete;
-  Mutex(Mutex&&) = delete;
   Mutex& operator=(const Mutex&) = delete;
-  Mutex& operator=(Mutex&&) = delete;
+
+  // Note: a moved-from mutex cannot be used.
+  Mutex(Mutex&& o);
+  Mutex& operator=(Mutex&& o);
 
   void Lock() {
     auto result = xSemaphoreTake(mutex_, portMAX_DELAY);
@@ -52,8 +59,11 @@ class Mutex {
     higher_priority_task_woken = pdTRUE == higher_priority_task_woken_raw;
   }
 
+  bool LockIsHeldByCurrentTask() const {
+    return xSemaphoreGetMutexHolder(mutex_) == xTaskGetCurrentTaskHandle();
+  }
+
  private:
-  StaticSemaphore_t storage_;
   SemaphoreHandle_t mutex_;
 };
 
@@ -68,6 +78,119 @@ class MutexLock {
 
  private:
   Mutex& mutex_;
+};
+
+// This type contains a resource and a mutex that protects it. The mutex is
+// already locked, and destroying the instance of this class will unlock it.
+// Therefore, while this class exists, the underlying resource can be safely
+// accessed from the current thread.
+template <typename T>
+class BorrowedPointer {
+ public:
+  BorrowedPointer(T* value, Mutex* mutex) : value_(value), mutex_(mutex) {
+    configASSERT(mutex_->LockIsHeldByCurrentTask());
+  }
+
+  template <typename U>
+  BorrowedPointer(BorrowedPointer<U>&& other) {
+    *this = std::forward<BorrowedPointer<U&&>>(other);
+  }
+
+  template <typename U>
+  BorrowedPointer<T>& operator==(BorrowedPointer<U>&& o) {
+    static_assert(std::is_convertible_v<U*, T*>);
+    value_ = o.value_;
+    mutex_ = o.mutex_;
+    o.mutex_ = nullptr;
+  }
+  ~BorrowedPointer() { mutex_->Unlock(); }
+
+  T* operator->() {
+    configASSERT(mutex_ != nullptr);
+    return value_;
+  }
+  T& operator*() {
+    configASSERT(mutex_ != nullptr);
+    return *value_;
+  }
+
+  T* value_;
+  Mutex* mutex_;
+};
+
+// Significantly inspired by Pigweed, but probably a little less flexible.
+// For example, we just use the above Mutex class. We'll probably migrate
+// someday to Pigweed if they get the Pico W to be fully supported.
+//
+// This is a pointer-like type and should be passed by value. It's a thing
+// that can be borrowed. The underlying object and mutex must outlive this.
+template <typename T>
+class Borrowable {
+ public:
+  Borrowable(T* value, Mutex* mutex) : value_(value), mutex_(mutex) {}
+  template <typename U>
+  Borrowable(U* value, Mutex* mutex) : value_(value), mutex_(mutex) {
+    static_assert(std::is_convertible_v<U*, T*>);
+  }
+
+  Borrowable(Borrowable<T>&& o) = default;
+  Borrowable& operator=(Borrowable<T>&& o) = default;
+
+  // Borrows the resource. Waits until the resource is available.
+  BorrowedPointer<T> Borrow() {
+    mutex_->Lock();
+    return BorrowedPointer<T>(value_, mutex_);
+  }
+
+  // Tries to borrow the resource. If it can't be borrowed by the time
+  // ms passes, returns nullopt.
+  std::optional<BorrowedPointer<T>> TryBorrow(int ms) {
+    if (!mutex_->LockWithTimeout(ms)) {
+      return std::nullopt;
+    }
+    return BorrowedPointer<T>(value_, mutex_);
+  }
+
+ private:
+  T* value_;
+  Mutex* mutex_;
+};
+
+// A borrowable that owns the resource.
+template <typename T>
+class OwnerBorrowable {
+ public:
+  template <typename... Args>
+  OwnerBorrowable(std::in_place_t, Args&&... args) {
+    value_ = std::make_unique<T>(std::forward<Args>(args)...);
+  }
+  explicit OwnerBorrowable(std::unique_ptr<T> value)
+      : value_(std::move(value)) {}
+  explicit OwnerBorrowable(T* value) : value_(value) {}
+
+  OwnerBorrowable(OwnerBorrowable<T>&& o) = default;
+  OwnerBorrowable& operator=(OwnerBorrowable<T>&& o) = default;
+
+  operator Borrowable<T>() { return Borrowable<T>(value_.get(), &mutex_); }
+
+  // Borrows the resource. Waits until the resource is available.
+  BorrowedPointer<T> Borrow() {
+    mutex_.Lock();
+    return BorrowedPointer<T>(value_, mutex_);
+  }
+
+  // Tries to borrow the resource. If it can't be borrowed by the time
+  // ms passes, returns nullopt.
+  std::optional<BorrowedPointer<T>> TryBorrow(int ms) {
+    if (!mutex_.LockWithTimeout(ms)) {
+      return std::nullopt;
+    }
+    return BorrowedPointer<T>(value_, mutex_);
+  }
+
+ private:
+  std::unique_ptr<T> value_;
+  Mutex mutex_;
 };
 
 }  // namespace freertosxx
--- src/freertosxx/mutex.cc
+++ src/freertosxx/mutex.cc
@@ -1,12 +1,22 @@
 #include "freertosxx/mutex.h"
 
+#include <utility>
+
 namespace freertosxx {
 
-Mutex::Mutex() { mutex_ = xSemaphoreCreateMutexStatic(&storage_); }
+Mutex::Mutex() { mutex_ = xSemaphoreCreateMutex(); }
 
 Mutex::~Mutex() {
+  if (mutex_ == nullptr) return;
   configASSERT(xSemaphoreGetMutexHolder(mutex_) == NULL);
   vSemaphoreDelete(mutex_);
 }
 
+Mutex::Mutex(Mutex&& o) { *this = std::forward<Mutex&&>(o); }
+
+Mutex& Mutex::operator=(Mutex&& o) {
+  configASSERT(xSemaphoreGetMutexHolder(o.mutex_) == nullptr);
+  mutex_ = std::exchange(o.mutex_, nullptr);
+  return *this;
+}
 }  // namespace freertosxx
